4e6fecc686e07a9b8b29b0b757363ec3d423fa4e
sskaf1 <sskaf@uwyo.edu>
2024-09-17 22:01:41 +0000

    lab02 final

---
+++ LAB-02-SSKAF2.py
@@ -0,0 +1,56 @@
+# Serena Skaf
+# UWYO COSC 1010
+# 09/17/2024
+# Lab 02 
+# Lab Section: 10
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+your_variable_here = "when you see this, replace it with your code"
+
+## Section ONE
+
+# Complete the following print statement to print out "Hello, COSC1010"
+print("HELLO, COSC1010")
+
+# Assign the string above to a variable named hello_message and print that variable
+hello_message = "HELLO, COSC1010"
+print(hello_message)
+
+# Assign the string "cowboy joe" to a variable, and print that variable with title casing
+Wyoming= "cowboy_joe"
+print(Wyoming.title())
+
+# Complete the following f-string print message 
+    # You will need to create your own variables and insert them  
+    # the final message should read `The University of Wyoming was founded in 1886`
+uwyo="University of Wyoming"
+founded="1886"
+print(f"The {uwyo} was founded in {founded}")
+
+# Now let's do some math with variables 
+    # Create two variables x and y and assign them the values 5 and 10 respectively 
+    # Complete the following print statements using your variables
+    #All math must be done within the braces in the f-strings
+x=5
+y=10
+print(f"x + y = {x+y}")
+print(f"x - y = {x-y}")
+print(f"x * y = {x*y}")
+print(f"x / y = {x/y}")
+
+# String concatenation 
+    # Finally we will take a look at string concatenation
+    # String concatenation combines two strings together
+    # It is done using the + operator
+    # Create three variables:
+        # first_name, which is your first name 
+        # last_name, which is your last name
+        # space, which is a space character 
+    # Use string concatenation to print out your full name 
+first_name= "Serena"
+last_name= "Skaf"
+space= " "
+print(f"{first_name}{space}{last_name}")
---
+++ lab01.py
@@ -0,0 +1,20 @@
+#Serena Skaf
+#UWYO COSC 1010
+#9/10/2024
+#01
+#10
+#Sources
+
+print("hello world")
+
+x= 5 # int
+y= 6.5 # float
+z= x+y # float
+print (z)
+
+first = "Serena"
+last = "Skaf"
+major = "biology"
+
+print("My name is: ", first,"",last)
+print("Major", major)
